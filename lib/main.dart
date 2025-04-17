423fc6fb2f744c77bdfe1baab0188ae62a4d957b
Edgar Martinez <edgar4ntonio_1999@hotmail.com>
2025-04-17 00:26:14 -0600

    Agregando el package hive_ce a la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Initialize hive_ce
+  await Hive.initFlutter();
+  // Open a box
+  // await Hive.openBox('nameBox');
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
